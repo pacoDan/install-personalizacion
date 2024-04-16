67758c2e5d2f997d3fde8c084dec6eccd8a166b6
pacoDan <jhonpaco@frba.utn.edu.ar>
2024-04-15 22:39:17 -0300

    :sparkles: add candy-icons

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 # Copiar los archivos .ttf a la carpeta de fuentes del sistema
-sudo cp -r JetBrainsMono/*.ttf /usr/share/fonts
+sudo cp -rf JetBrainsMono/*.ttf /usr/share/fonts
 
 # Actualizar la cache de fuentes del sistema
 sudo fc-cache -f -v
+
+# Copiar los iconos a la carpeta de iconos del sistema, reemplazando los existentes
+sudo cp -rf candy-icons /usr/share/icons
+
+# Actualizar la cache de iconos del sistema
+sudo gtk-update-icon-cache -f /usr/share/icons/candy-icons
