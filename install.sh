cb7e19b242255196a8e255eea5b165a8bf7761ed
Daniel <jhonpaco@frba.utn.edu.ar>
2025-04-02 20:36:54 -0300

    actualizo & font inconsolata

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Copiar los archivos .ttf a la carpeta de fuentes del sistema
 sudo cp -rf JetBrainsMono/*.ttf /usr/share/fonts
-
+sudo cp -rf InconsolataGo/*.ttf /usr/share/fonts
 # Actualizar la cache de fuentes del sistema
 sudo fc-cache -f -v
 
